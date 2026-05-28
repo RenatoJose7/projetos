ee563c9a32be535bb7354e3a9b28ba38d5cb442d
Renato <renatojosesfc@gmail.com>
2026-05-28 16:54:05 -0300

    .

--- carrossel/script.js
+++ carrossel/script.js
@@ -5,19 +5,24 @@ function mostrarImagens(){
     imagens.forEach(function(img){
         img.classList.remove("ativa")
     })
+
     imagens[indice].classList.add("ativa")
 }
+
 function proximo(){
     indice++
+
     if(indice >= imagens.length){
         indice = 0
     }
+
     mostrarImagens()
 }
-function voltar(){
-   indice--
 
-    if (indice < 0) {
+function voltar(){
+    indice--
+
+    if(indice < 0){
         indice = imagens.length - 1
     }
 
@@ -25,12 +30,27 @@ function voltar(){
 }
 
 
-function avancarAutomatico(){
-    indice++
-    if(indice >= imagens.length){
-        indice = 0
-    }
-    mostrarImagens()
+
+
+let imagens2 = document.querySelectorAll(".img1")
+let indiceFilme = 0
+
+function mostrarFilmes(){
+    imagens2.forEach(function(img){
+        img.classList.remove("ativa")
+    })
+
+    imagens2[indiceFilme].classList.add("ativa")
 }
 
-setInterval(avancarAutomatico, 2500)
+function passar(){
+    indiceFilme++
+
+    if(indiceFilme >= imagens2.length){
+        indiceFilme = 0
+    }
+
+    mostrarFilmes()
+}
+
+setInterval(passar, 2500)
